a50e39692b1ad9d3756e7ce7dee8be260228382b
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-02-09 23:09:47 -0700

    Add buy price and conditions to shop item tooltips
    Resolves #4

--- UINPCShopPanel.cs
+++ UINPCShopPanel.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.UI;
+using Terraria;
 
 namespace QuiteEnoughRecipes;
 
@@ -22,9 +26,74 @@ public class UINPCShopPanel : UIAutoExtend
 
 		foreach (var entry in shop.ActiveEntries)
 		{
-			grid.Append(new UIItemPanel(entry.Item));
+			grid.Append(new UIShopItemPanel(entry));
 		}
 
 		Append(grid);
+	}
+}
+
+// Shows the price at the bottom.
+file class UIShopItemPanel : UIItemPanel
+{
+	string? _conditions = null;
+
+	public UIShopItemPanel(AbstractNPCShop.Entry entry) : base(entry.Item)
+	{
+		var conditionDescs = entry.Conditions
+			.Select(c => c.Description.Value)
+			.Where(d => !string.IsNullOrWhiteSpace(d));
+		_conditions = string.Join("\n", conditionDescs);
+	}
+
+	public override void ModifyTooltips(Mod mod, List<TooltipLine> tooltips)
+	{
+		if (DisplayedItem == null) { return; }
+
+		var coinIds = new int[4]{
+			ItemID.PlatinumCoin,
+			ItemID.GoldCoin,
+			ItemID.SilverCoin,
+			ItemID.CopperCoin
+		};
+		var coinValues = new int[4];
+
+		int val = DisplayedItem.value;
+		for (int i = 3; i >= 1; --i)
+		{
+			coinValues[i] = val % 100;
+			val /= 100;
+		}
+
+		coinValues[0] = val;
+
+		// This is "Buy price: ".
+		var priceText = $"{Lang.tip[50]}";
+		if (DisplayedItem.value == 0)
+		{
+			// no value.
+			priceText += $" {Lang.tip[51]}";
+		}
+		else
+		{
+			for (int i = 0; i < 4; ++i)
+			{
+				if (coinValues[i] > 0)
+				{
+					priceText += $" {coinValues[i]}[i:{coinIds[i]}]";
+				}
+			}
+		}
+
+		tooltips.Add(new(mod, "QER: buy price", priceText){
+			OverrideColor = Main.OurFavoriteColor
+		});
+
+		if (!string.IsNullOrWhiteSpace(_conditions))
+		{
+			tooltips.Add(new(mod, "QER: buy conditions", _conditions){
+				OverrideColor = Main.OurFavoriteColor
+			});
+		}
 	}
 }
