43218fad62ea18079ae8a161ded4f09cd42d84f3
the_ultimate_lamp <harper.stoeckle@gmail.com>
2025-02-11 13:19:07 -0700

    Fix formatting

--- QuiteEnoughRecipes.cs
+++ QuiteEnoughRecipes.cs
@@ -1,31 +1,32 @@
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
 using ReLogic.Content;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Terraria;
+using System;
 using Terraria.GameContent;
 using Terraria.ModLoader;
 using Terraria.UI;
+using Terraria;
 
 namespace QuiteEnoughRecipes;
 
 public class QuiteEnoughRecipes : Mod
 {
-    public static void DrawItemIcon(Item item, int context, SpriteBatch spriteBatch, Vector2 screenPositionForItemCenter, float scale, float sizeLimit, Color environmentColor)
-    {
-        LoadItemAsync(item.type);
-        ItemSlot.DrawItemIcon(item, context, spriteBatch, screenPositionForItemCenter, scale, sizeLimit, environmentColor);
-    }
+	public static void DrawItemIcon(Item item, int context, SpriteBatch spriteBatch,
+		Vector2 screenPositionForItemCenter, float scale, float sizeLimit, Color environmentColor)
+	{
+		LoadItemAsync(item.type);
+		ItemSlot.DrawItemIcon(item, context, spriteBatch, screenPositionForItemCenter, scale, sizeLimit, environmentColor);
+	}
 
-    public static void LoadItemAsync(int i)
-    {
-        if (TextureAssets.Item[i].State == AssetState.NotLoaded)
-        {
-            Main.Assets.Request<Texture2D>(TextureAssets.Item[i].Name, AssetRequestMode.AsyncLoad);
-        }
-    }
+	public static void LoadItemAsync(int i)
+	{
+		if (TextureAssets.Item[i].State == AssetState.NotLoaded)
+		{
+			Main.Assets.Request<Texture2D>(TextureAssets.Item[i].Name, AssetRequestMode.AsyncLoad);
+		}
+	}
 }
