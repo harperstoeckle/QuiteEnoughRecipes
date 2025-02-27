d82d0b73c5d2c72e31211748d9832254e3245843
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-02-27 01:20:48 -0700

    Use text buy prices instead of item icons

--- QuiteEnoughRecipes.cs
+++ QuiteEnoughRecipes.cs
@@ -6,7 +6,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System;
+using Terraria.GameContent.UI;
 using Terraria.GameContent;
+using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.UI;
 using Terraria;
@@ -43,4 +45,60 @@ public class QuiteEnoughRecipes : Mod
 	 * immediately after the name so that it's clear what mod the ingredient came from.
 	 */
 	public static string GetModTagText(Mod mod) => $"   [c/56665e:〈{mod.DisplayNameClean}〉]";
+
+	// Get text for the buy price of an item. This supports custom currencies.
+	public static string GetBuyPriceText(Item item)
+	{
+		int storeValue = item.GetStoreValue();
+
+		if (storeValue <= 0)
+		{
+			// No value.
+			var color = Colors.AlphaDarken(new Color(120, 120, 120));
+			return $"[c/{color.Hex3()}:{Lang.tip[51]}]";
+		}
+		else if (item.shopSpecialCurrency != -1)
+		{
+			var line = new string[1];
+			var curLine = 0;
+			CustomCurrencyManager.GetPriceText(item.shopSpecialCurrency, line, ref curLine,
+				storeValue);
+			return line[0];
+		}
+		else
+		{
+			return $"{Lang.tip[50]} {GetCoinValueText(storeValue)}";
+		}
+	}
+
+	/*
+	 * Creates colored coin text based on a given value as seen when reforging. Taken from
+	 * `Main.DrawInventory()`.
+	 */
+	public static string GetCoinValueText(int value)
+	{
+		var text = new StringBuilder();
+		var coins = Utils.CoinsSplit(value);
+		var copper = coins[0];
+		var silver = coins[1];
+		var gold = coins[2];
+		var platinum = coins[3];
+
+		void AddCoinText(Color color, int amount, int langIndex) =>
+			text.Append($"[c/{color.Hex3()}:{amount} {Lang.inter[langIndex].Value}] ");
+
+		if (platinum > 0)
+			AddCoinText(Colors.AlphaDarken(Colors.CoinPlatinum), platinum, 15);
+
+		if (gold > 0)
+			AddCoinText(Colors.AlphaDarken(Colors.CoinGold), gold, 16);
+
+		if (silver > 0)
+			AddCoinText(Colors.AlphaDarken(Colors.CoinSilver), silver, 17);
+
+		if (copper > 0)
+			AddCoinText(Colors.AlphaDarken(Colors.CoinCopper), copper, 18);
+
+		return text.ToString();
+	}
 }
--- UINPCShopPanel.cs
+++ UINPCShopPanel.cs
@@ -54,30 +54,8 @@ file class UIShopItemPanel : UIItemPanel
 
 		if (DisplayedItem == null) { return; }
 
-		long val = DisplayedItem.GetStoreValue();
-		(int Item, long Count)[] counts = CustomCurrencyManager.TryGetCurrencySystem(DisplayedItem.shopSpecialCurrency, out var customCurrency)
-			? CustomCurrencySplit(customCurrency, val)
-			: CoinCurrencySplit(val);
-
-		// This is "Buy price: ".
-		var priceText = $"{Lang.tip[50]}";
-		if (val == 0)
-		{
-			// no value.
-			priceText += $" {Lang.tip[51]}";
-		}
-		else
-		{
-			foreach (var (item, count) in counts)
-			{
-				if (count > 0)
-				{
-					priceText += $" {count}[i:{item}]";
-				}
-			}
-		}
-
-		tooltips.Add(new(mod, "QER: buy price", priceText){
+		var buyPriceText = QuiteEnoughRecipes.GetBuyPriceText(DisplayedItem);
+		tooltips.Add(new(mod, "QER: buy price", buyPriceText){
 			OverrideColor = Main.OurFavoriteColor
 		});
 
@@ -87,58 +65,5 @@ file class UIShopItemPanel : UIItemPanel
 				OverrideColor = Main.OurFavoriteColor
 			});
 		}
-	}
-
-	private static (int Item, long Count)[] CoinCurrencySplit(long price)
-	{
-		(int Item, long Count)[] counts = new int[4]{
-			ItemID.PlatinumCoin,
-			ItemID.GoldCoin,
-			ItemID.SilverCoin,
-			ItemID.CopperCoin
-		}.Select(item => (item, 0L)).ToArray();
-
-		for (int i = 3; i >= 1; --i)
-		{
-			counts[i].Count = price % 100;
-			price /= 100;
-		}
-
-		counts[0].Count = price;
-
-		return counts;
-	}
-
-	[UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_valuePerUnit")]
-	extern static ref Dictionary<int, int> CustomCurrencySystem_valuePerUnit(CustomCurrencySystem self);
-
-	/*
-	 * Returns an array of (itemId, coin count) pairs representing how many of each item fulfil the price
-	 * The array is ordered from greatest to least currency value
-	 * Note: This uses a greedy approach and is not guaranteed to be the most optimal split of coins for non-canonical currencies
-	 */
-	private static (int Item, long Count)[] CustomCurrencySplit(CustomCurrencySystem currency, long price)
-	{
-		var currencyValues = CustomCurrencySystem_valuePerUnit(currency);
-
-		(int Item, long Count)[] counts = currencyValues.Select(v => (v.Key, 0L)).ToArray();
-		if (price == 0) { return counts; }
-
-		int index = 0;
-		foreach (var (item, worth) in currencyValues.OrderByDescending(v => v.Value))
-		{
-			var amount = price / worth;
-			price %= amount;
-			counts[index++].Count = amount;
-		}
-
-		// If this happens then the currency system is not canonical
-		// so we add 1 to the smallest unit thus overpaying, but satisfying the cost
-		if (price > 0)
-		{
-			counts[^1].Count++;
-		}
-
-		return counts;
 	}
 }
