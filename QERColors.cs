aef2c41e1fbc8d7ba5b4c8171064d8450a2b51fa
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-05-12 15:52:55 -0600

    Use the QER logo brown colors for window backgrounds for now

---
+++ QERColors.cs
@@ -0,0 +1,9 @@
+using Microsoft.Xna.Framework;
+
+namespace QuiteEnoughRecipes;
+
+public static class QERColors
+{
+	public static readonly Color Brown = new Color(52, 46, 52);
+	public static readonly Color DarkBrown = new Color(25, 21, 43);
+}
