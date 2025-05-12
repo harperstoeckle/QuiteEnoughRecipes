78c70e7315d03444e4aca3728eada346739cf8fb
Harper Stoeckle <harper.stoeckle@gmail.com>
2025-05-11 18:02:47 -0600

    Prevent the window manager child list from being modified during iteration

--- UIPopupWindow.cs
+++ UIPopupWindow.cs
@@ -7,37 +7,57 @@ namespace QuiteEnoughRecipes;
 // Window that appears at the cursor when opened and closes when the cursor leaves.
 public class UIPopupWindow : UIWindow
 {
+	private bool _wasJustOpened = false;
+
 	public void Open()
 	{
 		UISystem.WindowManager?.Open(this);
 
-		var dims = GetParentDimensions();
-
-		var mousePos = Main.MouseScreen - dims.Position();
-		var popupSize = GetOuterDimensions().ToRectangle().Size();
-
-		float xOffset = 15;
-		float yOffset = 50;
-		var pos = mousePos - new Vector2(popupSize.X - xOffset, yOffset);
-
-		if (pos.X < 0)
-		{
-			pos.X = mousePos.X - xOffset;
-		}
-		if (pos.Y + popupSize.Y > dims.Height)
-		{
-			pos.Y = dims.Height - popupSize.Y;
-		}
-
-		Left.Pixels = pos.X;
-		Top.Pixels = pos.Y;
-
-		Recalculate();
+		/*
+		 * Since `UIWindowManager::Open` defers adding this window to the next frame, we also have
+		 * to defer setting the initial position.
+		 */
+		_wasJustOpened = true;
 	}
 
 	protected override void DrawSelf(SpriteBatch sb)
 	{
+		if (_wasJustOpened)
+		{
+			var dims = GetParentDimensions();
+
+			var mousePos = Main.MouseScreen - dims.Position();
+			var popupSize = GetOuterDimensions().ToRectangle().Size();
+
+			float xOffset = 15;
+			float yOffset = 50;
+			var pos = mousePos - new Vector2(popupSize.X - xOffset, yOffset);
+
+			if (pos.X < 0)
+			{
+				pos.X = mousePos.X - xOffset;
+			}
+			if (pos.Y + popupSize.Y > dims.Height)
+			{
+				pos.Y = dims.Height - popupSize.Y;
+			}
+
+			Left.Pixels = pos.X;
+			Top.Pixels = pos.Y;
+
+			Recalculate();
+
+			_wasJustOpened = false;
+		}
+		else if (!ContainsPoint(Main.MouseScreen))
+		{
+			UISystem.WindowManager?.Close(this);
+		}
+
+		/*
+		 * Only draw *after* adjusting the position for the first time; otherwise, we draw one frame
+		 * of the window in the wrong spot.
+		 */
 		base.DrawSelf(sb);
-		if (!ContainsPoint(Main.MouseScreen)) { UISystem.WindowManager?.Close(this); }
 	}
 }
--- UIWindowManager.cs
+++ UIWindowManager.cs
@@ -1,4 +1,6 @@
+using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
+using System;
 using Terraria.UI;
 
 namespace QuiteEnoughRecipes;
@@ -12,23 +14,37 @@ public class UIWindowManager : UIState
 {
 	private Dictionary<string, UIWindow> _windows = new();
 
+	/*
+	 * In some cases (for example, `UIPopupWindow`, which closes itself during `DrawSelf`), a
+	 * window will attempt to modify the child list while it is being iterated over. Rather than
+	 * calling `RemoveChild` immediately, the call should be deferred until the start of the next
+	 * frame by appending it to this list.
+	 */
+	private List<Action> _deferredCalls = new();
+
 	public void AddWindow(string name, UIWindow w) => _windows.Add(name, w);
 
-	public void Open(UIWindow w)
-	{
-		if (!HasChild(w)) { Append(w); }
-	}
+	public void Open(UIWindow w) => _deferredCalls.Add(() => { if (!HasChild(w)) { Append(w); } });
+	public void Close(UIWindow w) => _deferredCalls.Add(() => RemoveChild(w));
 
-	public void Close(UIWindow w) => RemoveChild(w);
 	public bool IsOpen(UIWindow w) => HasChild(w);
 
 	public override void LeftMouseDown(UIMouseEvent e)
 	{
+		base.LeftMouseDown(e);
+
 		// Move the window to the top when it's clicked.
 		if (e.Target is UIWindow w)
 		{
 			RemoveChild(w);
 			Append(w);
 		}
+	}
+
+	protected override void DrawSelf(SpriteBatch sb)
+	{
+		foreach (var call in _deferredCalls) { call(); }
+		_deferredCalls.Clear();
+		base.DrawSelf(sb);
 	}
 }
